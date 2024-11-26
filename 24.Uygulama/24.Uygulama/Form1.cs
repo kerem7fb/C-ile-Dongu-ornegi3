be05795fe1c8f1073b3be9c9f85ed9fff80c4420
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-11-26 17:58:59 +0300

    Add files via upload

---
+++ 24.Uygulama/24.Uygulama/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24.Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam=0;
+            int baslangıc = Convert.ToInt32(textBox1.Text);
+            int bitis = Convert.ToInt32(textBox2.Text);
+            int artis = Convert.ToInt32(textBox3.Text);
+            for (int i = baslangıc; i <= bitis; i=i+artis)
+            {
+                toplam = toplam + i;
+            }
+            MessageBox.Show(bitis + " e kadar olan sayıların toplamı " + toplam);
+        }
+    }
+}
